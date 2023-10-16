c77662fd83c49eb9ac280beb2db7eae457a851bf
kongkong-std <zikang.qin95@gmail.com>
2023-10-16 02:21:17 -0500

    first commit

---
+++ sequential/src/iter_gmres.h
@@ -0,0 +1,9 @@
+#ifndef ITER_GMRES_H_
+#define ITER_GMRES_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#endif
---
+++ sequential/src/main.c
@@ -0,0 +1,8 @@
+#include "iter_gmres.h"
+
+int main(int argc, char **argv)
+{
+    puts( "sequential implementation GMRES..." );
+
+    return 0;
+}
